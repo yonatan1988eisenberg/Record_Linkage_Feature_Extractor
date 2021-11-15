59cadab8fc3934896169815f401d3e7251cc04da
Yonatan Eisenberg <yonatan.eis@gmail.com>
2021-11-15 15:30:59 +0200

    reparsed compare_cols and algos to compare_by and changed the defualt values, fixed sorting before removing duplicates

--- Feature_Extractor.py
+++ Feature_Extractor.py
@@ -24,7 +24,7 @@ config_object = ConfigParser()
 config_object.read("config.ini")
 
 
-def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+def progress_bar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
     """
     Call in a loop to create terminal progress bar,
     taken from https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
@@ -38,18 +38,19 @@ def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='�
         printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
     """
     total = len(iterable)
+
     # Progress Bar Printing Function
-    def printProgressBar(iteration):
+    def print_progress_bar(iteration):
         percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
-        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '-' * (length - filled_length)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
     # Initial Call
-    printProgressBar(0)
+    print_progress_bar(0)
     # Update Progress Bar
     for i, item in enumerate(iterable):
         yield item
-        printProgressBar(i + 1)
+        print_progress_bar(i + 1)
     # Print New Line on Complete
     print()
 
@@ -59,17 +60,19 @@ def compare_n_compute(df, candidate_pairs, args, logger):
     This function initiates the comparing algorithm and calculates the features. it returns the extracted features
     dataframe.
     """
-
     dfs_list = []
-    for al in progressBar(args.algos, prefix='Progress:', suffix='Complete', length=50):
-        logger.info(f'Comuting for algorithm {al}')
 
-        for col in progressBar(args.compare_cols, prefix='Progress:', suffix='Complete', length=50):
-            compare = rl.Compare()
-            compare.string(col, col, al, label=f'{col}_{al}')
-            dfs_list.append(compare.compute(candidate_pairs, df))
+    for dict_item in progress_bar(args.compare_by, prefix='Progress:', suffix='Complete', length=50):
+        for col, algo_list in dict_item.items():
+            logger.info(f'Computing for column {col}')
 
-        time.sleep(0.1)
+            for al in algo_list:
+                compare = rl.Compare()
+                compare.string(col, col, al, label=f'{col}_{al}')
+                dfs_list.append(compare.compute(candidate_pairs, df))
+
+            time.sleep(0.1)
+
     logger.info('Finished computing')
     return pd.concat(dfs_list, axis=1)
 
@@ -81,11 +84,13 @@ def index_df(df, args, logger):
 
     indexer = rl.Index()
 
-    for method, col in args.index_by.items():
-        if method in ['Block', 'SortedNeighbourhood']:
-            eval(f'indexer.add(rl.index.{method}({col}))')
-        else:
-            eval(f'indexer.add(rl.index.{method}())')
+    for dict_item in args.index_by:
+        for method, col_list in dict_item.items():
+            if method in ['Block', 'SortedNeighbourhood']:
+                for col in col_list:
+                    eval(f'indexer.add(rl.index.{method}({col}))')
+            else:
+                eval(f'indexer.add(rl.index.{method}())')
 
     candidate_pairs = indexer.index(df)
 
@@ -103,16 +108,17 @@ def preprocess_raw_df(raw_df, args, logger):
     # refactor Nan values in the about feature
     prcs_df['about'] = prcs_df.about.apply(lambda x: np.nan if x == 'unavailable' else x)
 
-    # lower case columns we'll compare on
-    for col in args.compare_cols:
-        prcs_df[col] = clean(prcs_df[col])
+    # lower case the columns we'll compare on
+    for dict_item in args.compare_by:
+        for col, algo_list in dict_item.items():
+            prcs_df[col] = clean(prcs_df[col])
 
     # remove duplicates if requested
     if args.remove_duplicates:
 
         # sort by sort_cols
-        asc = (False for _ in args.sort_cols)
-        prcs_df = prcs_df.sort_values(args.sort_cols, ascending=asc)
+        asc = [False for _ in args.sort_cols]
+        prcs_df.sort_values(args.sort_cols, ascending=asc, inplace=True)
 
         # remove duplicates by drop_cols
         prcs_df.drop_duplicates(subset=args.drop_cols, inplace=True)
@@ -123,21 +129,38 @@ def preprocess_raw_df(raw_df, args, logger):
 
 def val_data(raw_df, args, logger):
     """
-    This function validates that the columns names in the input data appears in the raw dataset.
+    This function validates that the columns names in the input data appears in the raw dataset and that the input
+    algorithms are supported.
     """
 
-    # validate sort_cols, drop_cols and compare_cols
-    for input_col in set(args.sort_cols + args.drop_cols + args.compare_cols):
+    # validate sort_cols, drop_cols
+    for input_col in set(args.sort_cols + args.drop_cols):
         if input_col not in raw_df.columns:
-            logger.debug('The columns used as input doesn\'t match the dataset')
+            logger.debug('The column(s) used as input in the parameter \'sort_cols\' or \'drop_cols\' doesn\'t '
+                         'match the dataset')
             exit()
 
     # validate index_by
-    for method, col in args.index_by.items():
-        if method in ['Block', 'SortedNeighbourhood']:
+    for item_dict in args.index_by:
+        for method, col_list in item_dict.items():
+            if method in ['Block', 'SortedNeighbourhood']:
+                for col in col_list:
+                    if col not in raw_df.columns:
+                        logger.debug('The column(s) used as input in the parameter \'index_by\' doesn\'t '
+                                     'match the dataset')
+                        exit()
+
+    # validate compare_by
+    for item_dict in args.compare_by:
+        for col, algo_list in item_dict.items():
             if col not in raw_df.columns:
-                logger.debug('The columns used as input doesn\'t match the dataset')
+                logger.debug('The column(s) used as input in the parameter \'compare_by\' doesn\'t match the dataset')
                 exit()
+
+            for algo in algo_list:
+                if algo not in config_object['const']['supported_algorithms'].split():
+                    logger.debug('An algorithm used as input isn\'t supported')
+                    exit()
 
     logger.info('Validated input columns match the dataset')
 
@@ -209,18 +232,13 @@ def parse_args(args, logger):
 
     my_parser.add_argument('--index_by', '-ib',
                            default=config_object['params']['index_by'],
-                           type=lambda v: {k: v for k, v in (x.split(':') for x in v.split(','))},
-                           help=config_object["params"]["index_by_help"])
+                           type=lambda arg: {pair.split(':')[0]: pair.split(':')[1].split(',') for pair in arg.split()},
+                           nargs='+', help=config_object["params"]["index_by_help"])
 
-    my_parser.add_argument('--compare_cols', '-cc',
-                           default=config_object['params']['compare_cols'].split(),
-                           type=str, nargs='+',
-                           help=config_object["params"]["compare_cols_help"])
-
-    my_parser.add_argument('--algos', '-al',
-                           default=config_object['params']['algos'].split(),
-                           type=str, nargs='+',
-                           help=config_object["params"]["algos_help"])
+    my_parser.add_argument('--compare_by', '-cb',
+                           default=config_object['params']['compare_by'],
+                           type=lambda arg: {pair.split(':')[0]: pair.split(':')[1].split(',') for pair in arg.split()},
+                           nargs='+', help=config_object["params"]["compare_by_help"])
 
     logger.info('Parsed arguments')
     return my_parser.parse_args()
@@ -231,10 +249,11 @@ def init_logger():
     This function initialize the logger and returns its handle
     """
     # todo: put format string in the config file
+
     log_formatter = logging.Formatter('%(levelname)s-%(asctime)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s')
     logger = logging.getLogger('log')
     logger.setLevel('DEBUG')
-    file_handler = logging.FileHandler('log.txt')
+    file_handler = logging.FileHandler('Feature_Extractor_log.txt')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(log_formatter)
     logger.addHandler(file_handler)
@@ -251,17 +270,22 @@ def main():
     # parse args
     args = parse_args(sys.argv[1:], logger)
     args.city = '_'.join(args.city)
+    if isinstance(args.index_by, dict):
+        args.index_by = [args.index_by]
     logger.info(f'args={args}')
 
     # get the data
     raw_df = get_data(args, logger)
+    save_path = config_object['const']['data_path_prefix'] + args.city + '/raw_df.csv'
+    raw_df.to_csv(save_path)
 
     # validate the data match input parameters
     val_data(raw_df, args, logger)
 
     # preprocess the data
     df = preprocess_raw_df(raw_df, args, logger)
-
+    save_path = config_object['const']['data_path_prefix'] + args.city + '/df.csv'
+    df.to_csv(save_path)
     # get candidate pairs
     candidate_pairs = index_df(df, args, logger)
 
@@ -278,4 +302,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
